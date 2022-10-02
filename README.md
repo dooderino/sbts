d4a1327773a31651245f45a5eaeb4c2b57180943
Joe Spataro <jspataro@gmail.com>
2022-10-02 01:19:33 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Seekers Beyond the Shroud for FoundryVTT!
+
+Implements basic character and item sheets for playing [Seekers Beyond the Shroud](https://www.exaltedfuneral.com/products/seekers-beyond-the-shroud) by [Blackoath Entertainment](https://blackoathgames.com) in Foundry VTT. Seekers Beyond the Shroud is a solo RPG set in modern day London where occult orders and sects vie for favor with demons by performing rituals.
+
+## Installation - Auto Installer (Recommended)
+
+1. In the FVTT Game Systems Menu, click `Install System`
+2. Search for "Seekers Beyond the Shroud" in the package search filter.
+3. After the world is created, import each of the compendium gear, table and macro packs.
+4. Grant player Observer permissions to the macro and to the items imported so they can view items clicked in chat.
+5. Allow player permissions to "creator actor".
+
+## Installation - Manual
+
+1. In the FVTT Game Systems Menu, click `Install System`
+2. Enter the Manifest URL: `path to system.json on raw github user content url`
+3. Follow steps 3-5 in the Auto Installer above.
+
+## Contributing
+
+If you want to contribute to this sheet, you'll need to clone this repository in the `systems` directory in Foundry VTT data path.
+
+Please note that the directory needs to be named `sbts` in order to be properly detected by Foundry VTT (i.e. it needs to look like `Data\systems\sbts`).
---
+++ sbts.mjs
@@ -0,0 +1,84 @@
+/**
+ * Seekers Beyond the Shroud game system for Foundry Virtual Tabletop
+ * Author: dooderino
+ * Software License: MIT
+ * Repository: https://github.com/dooderino/sbts
+ * Issue Tracker: https://github.com/dooderino/sbts/issues
+ */
+
+// Import Configuration
+import SBTS from "./module/config.mjs";
+
+// Import Submodules
+
+/* -------------------------------------------- */
+/*  Define Module Structure                     */
+/* -------------------------------------------- */
+
+globalThis.sbts = {};
+
+/* -------------------------------------------- */
+/*  Foundry VTT Initialization                  */
+/* -------------------------------------------- */
+
+Hooks.once("init", function () {
+  globalThis.sbts = game.sbts = Object.assign(game.system, globalThis.sbts);
+  console.log(
+    `SBtS | Initializing the SBtS Game System - Version ${sbts.version}\n${SBTS.ASCII}`
+  );
+});
+
+/* -------------------------------------------- */
+/*  Foundry VTT Setup                           */
+/* -------------------------------------------- */
+
+/**
+ * Prepare attribute lists.
+ */
+Hooks.once("setup", function () {});
+
+/* --------------------------------------------- */
+
+/**
+ * Expand a list of attribute paths into an object that can be traversed.
+ * @param {string[]} attributes  The initial attributes configuration.
+ * @returns {object}  The expanded object structure.
+ */
+function expandAttributeList(attributes) {
+  return attributes.reduce((obj, attr) => {
+    foundry.utils.setProperty(obj, attr, true);
+    return obj;
+  }, {});
+}
+
+/* --------------------------------------------- */
+
+/**
+ * Perform one-time pre-localization and sorting of some configuration objects
+ */
+//Hooks.once("i18nInit", () => utils.performPreLocalization(CONFIG.DND5E));
+
+/* -------------------------------------------- */
+/*  Foundry VTT Ready                           */
+/* -------------------------------------------- */
+
+/**
+ * Once the entire VTT framework is initialized, check to see if we should perform a data migration
+ */
+Hooks.once("ready", function () {});
+
+/* -------------------------------------------- */
+/*  Canvas Initialization                       */
+/* -------------------------------------------- */
+
+Hooks.on("canvasInit", (gameCanvas) => {});
+
+/* -------------------------------------------- */
+/*  Other Hooks                                 */
+/* -------------------------------------------- */
+
+/* -------------------------------------------- */
+/*  Bundled Module Exports                      */
+/* -------------------------------------------- */
+
+export { SBTS };
